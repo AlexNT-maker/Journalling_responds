4f459fa47f0c5d7d5d40e00b503360c7043c6664
AlexNT-maker <alexnter98@gmail.com>
2025-07-31 17:39:01 +0300

    Journal render page has been added, also CSS and button from UIverse has been added. I have change and the blockquote to be displayed by a card from UIverse design in the home page. Dummy data for testing has been added, until i will find which APIs will be used for the purposes of the project.

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+def respond_as_therapist(text):
+    """Dummy answer for testing"""
+    return "I understand you, sometime is okay to not feel good, actually is more than just okay."
+    
 
 @app.route('/') 
 def home(): 
     """Render the home page."""
     return render_template('index.html')
+
+
+@app.route('/journal', methods = ['GET', 'POST'])
+def journal():
+    """Render the journal page."""
+    ai_response = None 
+    if request.method == 'POST':
+        user_input = request.form.get('entry')
+        ai_response = respond_as_therapist(user_input)
+    return render_template('journal.html', ai_response = ai_response)
+
 
 if __name__ == '__main__': 
     app.run(debug=True)
